a5e1506b0100e63fa8722f7e7aae7658303f72fa
Marijn Ophorst <marijn@sensimedia.nl>
2015-09-01 07:57:49 +0200

    check for arrayness too

--- src/Dabble.php
+++ src/Dabble.php
@@ -28,6 +28,7 @@ trait Dabble
             foreach ($this->properties($this) as $prop) {
                 if (property_exists($this, $prop)
                     && !isset($annotations[$prop]['Virtual'])
+                    && !is_array($this->$prop)
                 ) {
                     $fields[] = $prop;
                 }
